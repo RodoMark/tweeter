16f8d58e52527e9a41dfb55c3e4d795354edcea6
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-26 19:16:44 +0000

    Added character counter to tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  console.log("Character counter: Ready");
+
+  $("#tweet-text").on("input", function (event) {
+    const $newTweet = $(event.target).closest("section");
+    const $tweetText = $newTweet.find("#tweet-text");
+    const $counter = $newTweet.find(".counter");
+
+    console.log($tweetText);
+
+    let count = $tweetText.val().length;
+    $counter.html(140 - count);
+  });
+});
