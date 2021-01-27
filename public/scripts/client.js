144686ab9ac05b47d4d899f42905bcc37a4cc81e
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-27 19:24:57 +0000

    Added POST that loads tweets from the database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,8 +5,28 @@
  */
 
 $(document).ready(function () {
-  $(".btn").on("submit", function (e) {
-    e.preventDefault();
+  $("#form").on("submit", function (event) {
+    event.preventDefault();
+    console.log("Button clicked, AJAX submitted");
+    const $form = $(this);
+    const $newTweet = $form.serialize();
+    const $tweetText = $("#tweet-text");
+
+    if ($tweetText.val().length > 140) {
+      const $counter = this.find(".counter");
+      $counter.insterBefore;
+    } else if (
+      $tweetText.text() === null ||
+      $tweetText.text() === null ||
+      $tweetText.text() === " "
+    ) {
+    } else {
+      $.ajax({
+        url: "http://localhost:8080/tweets",
+        method: "POST",
+        data: $newTweet,
+      });
+    }
   });
 
   const getDaysBetween = function (date1, date2) {
@@ -23,32 +43,6 @@ $(document).ready(function () {
     return Math.round(timeDifference / oneDay);
   };
 
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   const createTweetElement = function (tweetObj) {
     const article = `
     <article class="tweet">
@@ -63,13 +57,13 @@ $(document).ready(function () {
               <p>${tweetObj.content.text}</p>
               <footer>
                 <h6>Created ${getDaysBetween(
-                  new Date(),
-                  tweetObj.created_at
+                  tweetObj.created_at,
+                  new Date()
                 )} ago</h6>
                 <div class="socials">
-                  <img src="public/images/flags.svg">
-                  <img src="public/images/retweet.svg">
-                  <img src="public/images/heart.svg">
+                  <i class="fas fa-flag"></i>
+                  <i class="fas fa-retweet"></i>
+                  <i class="fas fa-heart"></i>
                 </div>
   
               </footer>
@@ -81,31 +75,20 @@ $(document).ready(function () {
   };
 
   const renderTweets = function (tweetObjects) {
-    tweetObjects.forEach(function (x) {
-      let $tweet = createTweetElement(x);
+    tweetObjects.forEach(function (obj) {
+      let $tweet = createTweetElement(obj);
       $("#tweet-section").append($tweet);
     });
   };
 
-  renderTweets(data);
+  const loadTweets = function () {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+    }).then(function (tweets) {
+      renderTweets(tweets.reverse());
+    });
+  };
 
-  // // Test / driver code (temporary). Eventually will get this from the server.
-  // const tweetData = {
-  //   user: {
-  //     name: "Newton",
-  //     avatars: "https://i.imgur.com/73hZDYK.png",
-  //     handle: "@SirIsaac",
-  //   },
-  //   content: {
-  //     text:
-  //       "If I have seen further it is by standing on the shoulders of giants",
-  //   },
-  //   created_at: 1461116232227,
-  // };
-
-  // const $tweet = createTweetElement(tweetData);
-
-  // // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  // $("#tweet-section").append($tweet);
+  loadTweets();
 });
