3e3a55042e7c86a117b9e7ebe5d104f5259b1d2a
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-27 14:45:21 +0000

    Added tweet rendering function CSS works now too

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,9 +5,46 @@
  */
 
 $(document).ready(function () {
+  $(function () {
+    const $button = $(". btn");
+    $button.on("click", function () {
+      console.log("Button clicked, performing ajax call...");
+      $.ajax("", { method: "GET" }).then(function (morePostsHtml) {
+        console.log("Success: ", morePostsHtml);
+        $button.replaceWith(morePostsHtml);
+      });
+    });
+  });
+
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
   const createTweetElement = function (tweetObj) {
     const article = `
-    <article>
+    <article class="tweet">
     <header>
                 <figure>
                   <img src=${tweetObj.user.avatars}>
@@ -18,7 +55,7 @@ $(document).ready(function () {
               </header>
               <p>${tweetObj.content.text}</p>
               <footer>
-                <h6>Created ${tweetObj.created_at}</h6>
+                <h6>Created ${tweetObj.created_at} ago</h6>
                 <div class="socials">
                   <img src="public/images/flags.svg">
                   <img src="public/images/retweet.svg">
@@ -33,23 +70,32 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
+  const renderTweets = function (tweetObjects) {
+    tweetObjects.forEach(function (x) {
+      let $tweet = createTweetElement(x);
+      $("#tweet-section").append($tweet);
+    });
   };
 
-  const $tweet = createTweetElement(tweetData);
+  renderTweets(data);
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $("#tweet-section").append($tweet);
+  // // Test / driver code (temporary). Eventually will get this from the server.
+  // const tweetData = {
+  //   user: {
+  //     name: "Newton",
+  //     avatars: "https://i.imgur.com/73hZDYK.png",
+  //     handle: "@SirIsaac",
+  //   },
+  //   content: {
+  //     text:
+  //       "If I have seen further it is by standing on the shoulders of giants",
+  //   },
+  //   created_at: 1461116232227,
+  // };
+
+  // const $tweet = createTweetElement(tweetData);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $("#tweet-section").append($tweet);
 });
