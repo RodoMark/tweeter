f5711138ac9cdb539cc2795b46c9fa0d6c620baf
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-28 21:22:39 +0000

    fine tuned scroll up when not at top

--- public/scripts/new-tweet-slider.js
+++ public/scripts/new-tweet-slider.js
@@ -3,19 +3,21 @@ $(document).ready(function () {
 
   $("#slider-button").click(function (event) {
     event.preventDefault();
+
     const $newTweet = $(".new-tweet");
     const $tweetText = $("#tweet-text");
     const $toTheTop = $("html, body").animate({ scrollTop: "0px" }, 550);
-    console.log("slider pressed");
+
+    if ($(this).scrollTop() > 215) {
+      $toTheTop;
+    }
 
     if ($newTweet.hasClass("up")) {
-      $toTheTop;
       $newTweet.slideDown(400);
       $newTweet.removeClass("hidden");
       $newTweet.removeClass("up");
       $tweetText.focus();
     } else {
-      $toTheTop;
       $newTweet.slideUp(400);
       $newTweet.addClass("up");
       $tweetText.blur();
