b49c488ff82ef11182e11fb4eb2fe9bc1a9d963e
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-28 13:01:02 +0000

    Revised button click function, now it works

--- public/scripts/new-tweet-slider.js
+++ public/scripts/new-tweet-slider.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
   console.log("Button clicker: Ready");
-  const newTweetSlider = function () {
-    $("#slider-button").on("click", function (event) {
-      event.preventDefault();
 
-      console.log("Button clicked");
-    });
-  };
+  $("#slider-button").click(function (event) {
+    event.preventDefault();
+
+    console.log("Button clicked");
+  });
 });
